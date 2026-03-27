1025ec42bdc63a36e82c2b05547b3ddfc72a0761
iko <mail@iko.soy>
2026-03-27 07:54:25 +0300

    Fix server compilation by updating to fetch API and restructuring handlers for proper type checking

--- client/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/Skrepka.AssemblyInfo.fs
+++ client/obj/Debug/net10.0-maccatalyst/maccatalyst-arm64/Skrepka.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Skrepka")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a9574e59e12a30d49104267d8ee9179ae3be0978")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+346c12ecffe87438fdad84bea4dff973f4fc9eec")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Skrepka")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Skrepka")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
