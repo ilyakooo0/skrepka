5feb6662d18e21810599668984fe623f2fdc584c
iko <mail@iko.soy>
2026-04-19 02:11:08 +0300

    Replace ListBox with ScrollViewer+VStack to fix conversation view not updating on new messages

--- client/ViewConversations.fs
+++ client/ViewConversations.fs
@@ -32,51 +32,51 @@ module ViewConversations =
                 )
             else
                 AnyView(
-                    ListBox(
-                        contacts,
-                        fun c ->
-                            let name = contactName model.Contacts c.Pubkey
+                    ScrollViewer(
+                        VStack(0.) {
+                            for c in contacts do
+                                let name = contactName model.Contacts c.Pubkey
 
-                            let preview =
-                                messagesFor c.Pubkey model.Messages
-                                |> List.tryLast
-                                |> Option.map (fun m -> if m.Body.Length > 40 then m.Body.[..39] + "..." else m.Body)
-                                |> Option.defaultValue ""
+                                let preview =
+                                    messagesFor c.Pubkey model.Messages
+                                    |> List.tryLast
+                                    |> Option.map (fun m -> if m.Body.Length > 40 then m.Body.[..39] + "..." else m.Body)
+                                    |> Option.defaultValue ""
 
-                            Border(
-                                (HStack(8.) {
+                                Border(
+                                    (HStack(8.) {
 
-                                    Grid() {
-                                        (match c.Photo with
-                                         | None -> Image("avares://Skrepka/Assets/Images/user.png", Stretch.Uniform)
-                                         | Some i -> Image(cachedBitmap i, Stretch.UniformToFill))
-                                            .width(16.)
-                                            .height(16.)
-                                            .margin(8.)
-                                            .clipToBounds (true)
+                                        Grid() {
+                                            (match c.Photo with
+                                             | None -> Image("avares://Skrepka/Assets/Images/user.png", Stretch.Uniform)
+                                             | Some i -> Image(cachedBitmap i, Stretch.UniformToFill))
+                                                .width(16.)
+                                                .height(16.)
+                                                .margin(8.)
+                                                .clipToBounds (true)
 
-                                        Rectangle().stroke(Colors.Black).strokeThickness (4.)
-                                    }
+                                            Rectangle().stroke(Colors.Black).strokeThickness (4.)
+                                        }
 
-                                    VStack(2.) {
-                                        TextBlock(name)
-                                            .fontFamily(Constants.fontFamily)
-                                            .fontWeight(FontWeight.Bold)
-                                            .fontSize (16.)
+                                        VStack(2.) {
+                                            TextBlock(name)
+                                                .fontFamily(Constants.fontFamily)
+                                                .fontWeight(FontWeight.Bold)
+                                                .fontSize (16.)
 
-                                        if preview <> "" then
-                                            TextBlock(preview).fontSize(12.).fontWeight (FontWeight.Bold)
-                                    }
+                                            if preview <> "" then
+                                                TextBlock(preview).fontSize(12.).fontWeight (FontWeight.Bold)
+                                        }
 
-                                })
-                                    .margin (8.)
-                            )
-                                .borderThickness(Avalonia.Thickness(0., 0., 0., 4.))
-                                .borderBrush(SolidColorBrush(Colors.Black))
-                                .background(Colors.Transparent)
-                                .onTapped (fun _ -> SetPage(Chat(c.Pubkey, "")))
+                                    })
+                                        .margin (8.)
+                                )
+                                    .borderThickness(Avalonia.Thickness(0., 0., 0., 4.))
+                                    .borderBrush(SolidColorBrush(Colors.Black))
+                                    .background(Colors.Transparent)
+                                    .onTapped (fun _ -> SetPage(Chat(c.Pubkey, "")))
+                        }
                     )
-                        .styles(noListBoxPadding ())
                         .background(Colors.White)
                         .verticalAlignment (VerticalAlignment.Bottom)
                 )
