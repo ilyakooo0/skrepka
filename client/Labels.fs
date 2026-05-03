05f993103d601d45a0bd0cb5c972631fa6eb6461
iko <mail@iko.soy>
2026-05-03 13:01:26 +0300

    Update effect annotations from reads/writes to r/w/rw syntax; format Labels.fs

--- client/Labels.fs
+++ client/Labels.fs
@@ -12,12 +12,16 @@ module Labels =
             .fontFamily(FontFamily(Constants.fontFamily))
             .fontWeight(FontWeight.Bold)
             .fontSize(size)
-            .textWrapping(TextWrapping.Wrap)
+            .textWrapping (TextWrapping.Wrap)
 
     let h1 text = label 32. text
     let h2 text = label 24. text
     let h3 text = label 20. text
     let h4 text = label 16. text
     let h5 text = label 14. text
-    let body text = label 14. text |> _.fontWeight(FontWeight.Normal)
-    let caption text = label 12. text |> _.fontWeight(FontWeight.Normal)
+
+    let body text =
+        label 14. text |> _.fontWeight(FontWeight.Normal)
+
+    let caption text =
+        label 12. text |> _.fontWeight(FontWeight.Normal)
